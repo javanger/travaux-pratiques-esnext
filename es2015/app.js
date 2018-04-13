47adc39417d8e49eb3ed0adaa5f23daeadb182f0
victorahs <victorahsene90@gmail.com>
2018-04-13 16:45:02 +0200

    add ^map

--- es2015/app.js
+++ es2015/app.js
@@ -46,35 +46,40 @@ console.log(nycId);
 console.log(othersCitiesId.length);
 
 class Trip {
-    constructor(id, name, nameUrl){
-        this.id = id;
-        this.name = name;
-        this.nameUrl = nameUrl;
-    }
+  constructor(id, name, nameUrl) {
+    this.id = id;
+    this.name = name;
+    this.nameUrl = nameUrl;
+  }
 
-    toString(){
+  toString() {
+    return (
+      "Trip [" +
+      this.id +
+      " " +
+      this.name +
+      " " +
+      this.nameUrl +
+      " " +
+      this._price +
+      "]"
+    );
+  }
 
-        return 'Trip [' + this.id + ' ' + this.name + ' ' + this.nameUrl + ' ' + this._price + ']';
-    }
+  static getDefaultTrip() {
+    let id = "rio-de-janeiro";
+    let name = "Rio de Janeiro";
+    let nameUrl = "img/rio-de-janeiro.jpg";
+    return new Trip(id, name, nameUrl);
+  }
 
-    static getDefaultTrip(){
+  get price() {
+    return this._price;
+  }
 
-        let id = "rio-de-janeiro";
-        let name = "Rio de Janeiro";
-        let nameUrl = "img/rio-de-janeiro.jpg";
-        return new Trip(id,name, nameUrl);
-
-    }
-
-    get price(){
-        return this._price;
-    }
-
-    set price(newPrice) {
-        this._price = newPrice;
-    }
-
-
+  set price(newPrice) {
+    this._price = newPrice;
+  }
 }
 
 let parisTrip = new Trip("paris", "Paris", "img/paris.jpg");
@@ -90,29 +95,146 @@ const defaultTrip = Trip.getDefaultTrip();
 
 console.log(defaultTrip.toString());
 
-class FreeTrip extends Trip{
+class FreeTrip extends Trip {
+  constructor(id, name, nameUrl) {
+    super(id, name, nameUrl);
 
-    constructor(id, name, nameUrl){
-        super(id, name, nameUrl);
-       
-       this._price = 0;
+    this._price = 0;
+  }
 
-    }
-    
-    toString(){
-         return 'Free' + super.toString();
-    }
-    
-    }
-
-
-
-
+  toString() {
+    return "Free" + super.toString();
+  }
+}
 
 const freeTrip = new FreeTrip("nantes", "Nantes", "img/nantes.jpg");
 
 console.log(freeTrip.toString());
 
+class TripService {
+  constructor() {
+    // TODO Set of 3 trips
 
+    this.setTrips = new Set();
+    this.setTrips.add(new Trip("paris", "Paris", "img/paris.jpg"));
+    this.setTrips.add(new Trip("nantes", "Nantes", "img/nanges.jpg"));
+    this.setTrips.add(
+      new Trip("rio-de-janeiro", "Rio de Janeiro", "img/rio-de-janeiro.jpg")
+    );
+  }
 
+  findByName(tripName) {
+    // TODO return promise
 
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let tripTrouve = Array.from(this.setTrips).find(
+          t => t.name == tripName
+        );
+
+        if (tripTrouve) {
+          resolve(tripTrouve);
+        } else {
+          reject("No trip found " + tripName);
+        }
+      }, 2000);
+    });
+  }
+}
+
+class PriceService {
+  constructor() {
+    // TODO Map of 2 trips
+    // 'paris' --> price = 100
+    // 'rio-de-janeiro' --> price = 800)
+    // no price for 'nantes'
+
+    this.mapPriceService = new Map();
+    this.mapPriceService.set("paris", 100);
+    this.mapPriceService.set("rio-de-janeiro", 800);
+  }
+
+  findPriceByTripId(tripId) {
+    // TODO return promise
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let tripTrouve = this.mapPriceService.get(tripId);
+
+        if (tripTrouve) {
+          resolve(tripTrouve);
+        } else {
+          reject("No trip with id " + tripId);
+        }
+      }, 2000);
+    });
+  }
+}
+
+let testService = new TripService();
+
+let testPriceService = new PriceService();
+
+testService.findByName("Paris").then(
+  function(resultatQuandOk) {
+    // cas resolve
+    console.log("Trip trouvé", resultatQuandOk);
+  },
+  function(errorQuandKo) {
+    console.log(errorQuandKo);
+  }
+);
+
+testService.findByName("Toulouse").then(
+  function(id) {
+    console.log("Trip found : " + id);
+  },
+  function(error) {
+    console.log(error);
+  }
+);
+
+testService.findByName("Rio de Janeiro").then(
+  function(resultOk) {
+    console.log("Trip found : " + resultOk + "\n");
+
+    testPriceService.findPriceByTripId(resultOk.id).then(
+      function(priceOk) {
+        console.log("Price found : " + priceOk);
+      },
+      function(error) {
+        console.log("No price for trip id " + resultOk.id);
+      }
+    );
+  },
+  function(error) {
+    console.log(error);
+  }
+);
+
+testService.findByName("Toulouse").then(
+  function(id) {
+    console.log("Trip found : " + id);
+  },
+  function(error) {
+    console.log(error);
+  }
+);
+
+testService.findByName("Nantes").then(
+  function(resultOk) {
+    console.log("Trip found : " + resultOk + "\n");
+
+    testPriceService.findPriceByTripId(resultOk.id).then(
+      function(priceOk) {
+        console.log("Price found : " + priceOk);
+      },
+      function(error) {
+        console.log("No price for trip id " + resultOk.id);
+      }
+    );
+  },
+  function(error) {
+    console.log(error);
+  }
+);
